ac18c30a47a0f7b746904a9ee4173177a16de5a6
aliyasami <aliyasami205@gmail.com>
2025-07-18 09:37:38 +0330

    Add Bottom Navigation

--- lib/home.dart
+++ lib/home.dart
@@ -24,7 +24,42 @@ class _HomeState extends State<Home> {
             IconButton(
               onPressed: () {},
               icon: Icon(CupertinoIcons.shopping_cart),
-              color: Colors.black54,
+              color: Colors.black45,
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
+          elevation: 20,
+
+          selectedFontSize: 14,
+          selectedIconTheme: IconThemeData(color: Colors.teal, size: 30),
+          selectedItemColor: Colors.teal,
+          selectedLabelStyle: TextStyle(
+            color: Colors.blue,
+            fontFamily: 'iransans',
+            fontWeight: FontWeight.w600,
+          ),
+
+          unselectedIconTheme: IconThemeData(size: 30),
+          unselectedLabelStyle: TextStyle(
+            fontFamily: 'iransans',
+            fontWeight: FontWeight.w200,
+          ),
+
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.settings),
+              label: 'تنظیمات',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: 'صفحه اصلی',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.shopping_cart),
+              label: 'سبد خرید',
             ),
           ],
         ),
