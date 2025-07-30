c5d7412a4eea1e00b92413a60e1709997b959d35
aliyasami <aliyasami205@gmail.com>
2025-07-30 12:56:43 +0330

    fix some problem of shopping basket class

--- lib/ShoppingBasket.dart
+++ lib/ShoppingBasket.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home.dart';
 
 class Shoppingbasket extends StatefulWidget {
   const Shoppingbasket({super.key});
@@ -10,6 +12,40 @@ class Shoppingbasket extends StatefulWidget {
 class _ShoppingbasketState extends State<Shoppingbasket> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'سبد خرید شما ',
+            style: TextStyle(
+              fontFamily: 'iransans',
+              fontWeight: FontWeight.w600,
+              color: Colors.teal,
+            ),
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(CupertinoIcons.back, color: Colors.black),
+            onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Home(),)),
+          ),
+          backgroundColor: Colors.white,
+          elevation: 10,
+        ),
+        body:
+            shoppingBasketUi(context), // محصول به متد _descriptionUi ارسال می‌شود
+      ),
+    );;
   }
+  Widget shoppingBasketUi(context){
+    return Stack(
+
+      children: [
+
+      ],
+
+    );
+  }
+
+
 }
